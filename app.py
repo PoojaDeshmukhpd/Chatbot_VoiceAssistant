9835e51836ee9f0276eb70c5d5124da53d991e2d
Pooja Deshmukh <101447967+PoojaDeshmukhpd@users.noreply.github.com>
2023-11-25 21:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,5 +54,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0)
+    # app.run(debug=True)
     # wishMe()
